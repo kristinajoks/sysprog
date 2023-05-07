c834bf031e85e35305af79dbb08990e830a7e905
Kristina Joksimovic <joksimovic.kristina@elfak.rs>
2023-05-07 13:14:56 +0200

    Prvi projekat

--- 18203Proj1/Program.cs
+++ 18203Proj1/Program.cs
@@ -12,82 +12,6 @@ class Program
     static HttpListener listener = new HttpListener();
     static HttpClient client = new HttpClient();
 
-    //static void ServerThreadPool() 
-    //{
-    //    try //ukoliko je ovde, nakon prve nedostupne slike se zatvara program, ukoliko je unutar while radi i nakon
-    //    {
-    //        listener.Prefixes.Add("http://localhost:5050/");
-    //        listener.Start();
-
-    //        Console.WriteLine("Listening on port 5050...");
-
-    //        while (true)
-    //        {
-    //            try
-    //            {
-    //                HttpListenerContext context = listener.GetContextAsync().Result; //isto kao iznad zbog .Result
-    //                HttpListenerRequest request = context.Request;
-
-    //                Console.WriteLine($"Recieved request for {request.Url}");
-
-    //                if (context.Request.Url == null)
-    //                {
-    //                    throw new HttpRequestException();
-    //                }
-
-    //                string localPath = "C:\\Users\\krist\\Downloads\\3. godina\\SP\\18203Proj1\\photos\\";
-    //                string path = localPath + context.Request.Url.LocalPath;
-
-    //                HttpListenerResponse response = context.Response;
-    //                response.Headers.Set("Content-type", "image/jpg");
-
-    //                byte[] buffer = File.ReadAllBytes(path);
-
-    //                //odavde proba sa thread pool-om
-    //                int broj_niti = Environment.ProcessorCount;
-    //                Console.WriteLine($"Dostupan broj niti je: {broj_niti}");
-    //                int worker_niti, io_niti;
-    //                ThreadPool.GetAvailableThreads(out worker_niti, out io_niti);
-
-
-    //                //treba izmeniti kod tako da nekako prolazi po delovima kroz strukturu, nit izvrsava izmenu za deo
-    //                //i upisuje u zajednicku strukturu na definisano mesto
-    //                ThreadPool.QueueUserWorkItem((state) =>
-    //                {
-    //                    //ovako ce se nakon spajanja prikazati konacni rezultat
-    //                    Stream stream = response.OutputStream;
-
-    //                    string respath;
-
-    //                    Image image = Image.Load(path);
-    //                    using (image)
-    //                    {
-    //                        respath = Convert(path, image);
-    //                        byte[] res = File.ReadAllBytes(respath);
-    //                        response.ContentLength64 = res.Length;
-    //                        stream.Write(res, 0, res.Length);
-    //                    }
-    //                });
-
-    //                bool gotova_obrada = false;
-    //                while (!gotova_obrada)
-    //                {
-    //                    Thread.Sleep(1000);
-    //                    gotova_obrada = ThreadPool.PendingWorkItemCount == 0; //cekamo da obrada bude gotova
-    //                }                                       
-    //            }
-    //            catch(FileNotFoundException  ex)
-    //            {
-    //                Console.WriteLine(ex.Message);
-    //            }
-    //        }
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        Console.WriteLine(ex.Message);
-    //    }
-    //}
-
     public static void ServerDivided()
     {
         listener.Prefixes.Add("http://localhost:5050/");
@@ -99,7 +23,7 @@ class Program
         {
             try
             {
-                HttpListenerContext context = listener.GetContextAsync().Result; //isto kao iznad zbog .Result
+                HttpListenerContext context = listener.GetContextAsync().Result; 
                 HttpListenerRequest request = context.Request;
 
                 Console.WriteLine($"Recieved request for {request.Url}");
@@ -178,7 +102,6 @@ class Program
 
         return bmparray;
     }
-
     
     public static Bitmap[,] parallelImageProcess(Bitmap[,] bmp)
     {
@@ -216,8 +139,6 @@ class Program
         return bmp;
     }
 
-
-
     public static Bitmap joinTiles(Bitmap[,] bitmaps, Bitmap original)
     {
         Size tilesize = new Size(original.Width / 4, original.Height / 3);
@@ -242,86 +163,6 @@ class Program
 
         return res;
     }
-
-    //funkcija koja preuzima putanju slike i sliku, a vraca putanju crno-bele slike
-    //static string Convert(string path, Image image)
-    //{
-    //    RasterCachedImage rasterCachedImage = (RasterCachedImage)image;
-    //    if (!rasterCachedImage.IsCached)
-    //    {
-    //        rasterCachedImage.CacheData();
-    //    }
-    //    //sta ako jeste??
-
-    //    path = path.Substring(0, path.Length - 4);
-    //    string newpath = $"{path}grayscaled.jpg";
-    //    rasterCachedImage.Grayscale();
-    //    rasterCachedImage.Save(newpath);
-
-    //    return newpath;
-    //}
-
-    //static void Server()
-    //{
-    //    listener.Prefixes.Add("http://localhost:5050/");
-    //    listener.Start();
-
-    //    Console.WriteLine("Listening on port 5050...");
-
-    //    while (true)
-    //    {
-    //        try
-    //        {
-    //            //HttpListenerContext context = listener.GetContext(); //ceka zahtev i vraca njega kada pristigne
-    //            //HttpListenerRequest request = context.Request;
-
-    //            HttpListenerContext context = listener.GetContextAsync().Result; //isto kao iznad zbog .Result
-    //            HttpListenerRequest request = context.Request;
-
-    //            Console.WriteLine($"Recieved request for {request.Url}");
-
-    //            if (context.Request.Url == null)
-    //            {
-    //                throw new HttpRequestException();
-    //            }
-
-    //            string localPath = "C:\\Users\\krist\\Downloads\\3. godina\\SP\\18203Proj1\\photos\\";
-    //            string path = localPath + context.Request.Url.LocalPath;
-
-    //            HttpListenerResponse response = context.Response;
-    //            response.Headers.Set("Content-type", "image/jpg");
-
-    //            byte[] buffer = File.ReadAllBytes(path);
-    //            //response.ContentLength64 = buffer.Length;
-
-    //            //ovako ce se nakon spajanja prikazati konacni rezultat
-    //            Stream stream = response.OutputStream;
-    //            //stream.Write(buffer, 0, buffer.Length);
-
-
-    //            if(request.Url.AbsolutePath == "/favicon.ico")
-    //            {
-    //                stream.Write(buffer, 0, buffer.Length);
-    //                return;
-    //            }
-
-    //            string respath;
-
-    //            Image image = Image.Load(path);
-    //            using (image)
-    //            {
-    //                respath = Convert(path, image);
-    //                byte[] res = File.ReadAllBytes(respath);
-    //                response.ContentLength64 = res.Length;
-    //                stream.Write(res, 0, res.Length);
-    //            }
-    //        }
-    //        catch (FileNotFoundException ex)
-    //        {
-    //            Console.WriteLine(ex.Message);
-    //        }
-    //    }
-    //}
 
     static void Main(string[] args)
     {
