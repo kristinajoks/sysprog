a00a292c3ff738d2211a77da3a45889fdc289ccb
Kristina Joksimovic <joksimovic.kristina@elfak.rs>
2023-05-07 14:10:01 +0200

    Prvi projekat

---
+++ 18203Proj1/Cache.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18203Proj1
+{
+    public class LocalCache
+    {
+        private Dictionary<string, Bitmap> cache;
+
+        public LocalCache ()
+        {
+            this.cache = new Dictionary<string, Bitmap> ();
+        }
+
+        public bool containReq(string request)
+        {
+            if(this.cache.ContainsKey(request)) return true;
+            return false;
+        }
+
+        public void addReq(string request, Bitmap bmp) { 
+            this.cache.TryAdd(request, bmp);
+        }
+
+        public bool tryGetValue(string request, out Bitmap value)
+        {
+            bool status = this.cache.TryGetValue(request, out value);
+            return status;
+        }
+
+    }
+}
--- 18203Proj1/Program.cs
+++ 18203Proj1/Program.cs
@@ -1,167 +1,180 @@
 ﻿using System.Net;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 
-class Program
+namespace _18203Proj1
 {
-    static HttpListener listener = new HttpListener();
-    static HttpClient client = new HttpClient();
-
-    public static void ServerDivided()
+    class Program
     {
-        listener.Prefixes.Add("http://localhost:5050/");
-        listener.Start();
+        static HttpListener listener = new HttpListener();
+        static HttpClient client = new HttpClient();
+        static LocalCache cache = new LocalCache();
 
-        Console.WriteLine("Listening on port 5050...");
-
-        while (true)
+        public static void ServerDivided()
         {
-            try
+            listener.Prefixes.Add("http://localhost:5050/");
+            listener.Start();
+
+            Console.WriteLine("Listening on port 5050...");
+
+            while (true)
             {
-                HttpListenerContext context = listener.GetContextAsync().Result; 
-                HttpListenerRequest request = context.Request;
-
-                Console.WriteLine($"Recieved request for {request.Url}");
-
-                if (context.Request.Url == null)
+                try
                 {
-                    throw new HttpRequestException();
-                }
+                    HttpListenerContext context = listener.GetContextAsync().Result;
+                    HttpListenerRequest request = context.Request;
 
-                string localPath = "C:\\Users\\krist\\Downloads\\3. godina\\SP\\18203Proj1\\photos\\";
-                string path = localPath + context.Request.Url.LocalPath;
+                    Console.WriteLine($"Recieved request for {request.Url}");
 
-                HttpListenerResponse response = context.Response;
-                response.Headers.Set("Content-type", "image/jpg");
-
-                byte[] buffer = File.ReadAllBytes(path);
-
-                Stream stream = response.OutputStream;
-
-                if (request.Url.AbsolutePath == "/favicon.ico")
-                {
-                    stream.Write(buffer, 0, buffer.Length);
-                    return;
-                }
-
-                string respath;
-
-                Bitmap imgFile = new Bitmap(path);
-                Bitmap[,] tiles = makeTiles((object)imgFile);
-                Bitmap[,] res = parallelImageProcess(tiles);
-                Bitmap joined = joinTiles(res, imgFile);
-
-                
-
-                byte[] resArray = toByteArr(joined, System.Drawing.Imaging.ImageFormat.Bmp);
-                stream.Write(resArray, 0, resArray.Length);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-    }
-
-    public static byte[] toByteArr(Bitmap bitmap, System.Drawing.Imaging.ImageFormat format)
-    {
-        using (MemoryStream ms = new MemoryStream())
-        {
-            bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
-            return ms.ToArray();
-        }
-    }
-
-    public static Bitmap[,] makeTiles(object bmap)
-    {
-        Bitmap bmp = (Bitmap)bmap;
-        Size tilesize = new Size(bmp.Width / 4, bmp.Height / 3);
-        Bitmap[,] bmparray = new Bitmap[4, 3];
-
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                //srcTile
-                Rectangle movingTile = new Rectangle(i* tilesize.Width, j*tilesize.Height, tilesize.Width, tilesize.Height);
-
-                bmparray[i,j]= new Bitmap(tilesize.Width, tilesize.Height);
-
-                //ubacivanje slika u bmparray
-                using (Graphics canvas = Graphics.FromImage(bmparray[i, j]))
-                {
-                    canvas.DrawImage(bmp, new Rectangle(0, 0, tilesize.Width, tilesize.Height), movingTile, GraphicsUnit.Pixel);
-                }
-            }
-        }
-
-        return bmparray;
-    }
-    
-    public static Bitmap[,] parallelImageProcess(Bitmap[,] bmp)
-    {
-
-        int broj_niti = Environment.ProcessorCount;
-        Console.WriteLine($"Dostupan broj niti je: {broj_niti}");
-        int worker_niti, io_niti;
-        ThreadPool.GetAvailableThreads(out worker_niti, out io_niti);
-
-        foreach (Bitmap bitmap in bmp)
-        {
-            ThreadPool.QueueUserWorkItem((state) =>
-            {
-                lock (bitmap)
-                {
-                    int width = bitmap.Width;
-                    int height = bitmap.Height;
-
-                    for (int i = 0; i < width; i++)
+                    if (request.Url == null)
                     {
-                        for (int j = 0; j < height; j++)
-                        {
-                            Color oldPixel = bitmap.GetPixel(i, j);
+                        throw new HttpRequestException();
+                    }
 
-                            int grayScale = (int)((oldPixel.R * 0.229) + (oldPixel.G * 0.587) + (oldPixel.B * 0.114));
-                            Color newPixel = Color.FromArgb(grayScale, grayScale, grayScale);
-                            
-                            bitmap.SetPixel(i, j, newPixel);
+
+                    HttpListenerResponse response = context.Response;
+                    response.Headers.Set("Content-type", "image/jpg");
+
+                    Stream stream = response.OutputStream;
+
+                    Bitmap final;
+
+                    if (cache.containReq(request.Url.ToString()))
+                    {
+                        cache.tryGetValue(request.Url.ToString(), out final);
+                    }
+                    else
+                    {
+                        string localPath = "C:\\Users\\krist\\sysprog\\18203Proj1\\photos\\";
+                        string path = localPath + request.Url.LocalPath;
+
+                        byte[] buffer = File.ReadAllBytes(path);
+
+                        if (request.Url.AbsolutePath == "/favicon.ico") 
+                        {
+                            stream.Write(buffer, 0, buffer.Length);
+                            cache.addReq(request.Url.ToString(), new Bitmap(path)); //revise
+                            return;
                         }
+
+                        Bitmap imgFile = new Bitmap(path);
+                        Bitmap[,] tiles = makeTiles((object)imgFile);
+                        //Bitmap[,] res = parallelImageProcess(tiles);
+                        final = joinTiles(tiles, imgFile);
+
+                        cache.addReq(request.Url.ToString(), final);
+                    }
+
+                    byte[] resArray = toByteArr(final, System.Drawing.Imaging.ImageFormat.Bmp);
+                    stream.Write(resArray, 0, resArray.Length);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public static byte[] toByteArr(Bitmap bitmap, System.Drawing.Imaging.ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+                return ms.ToArray();
+            }
+        }
+
+        public static Bitmap[,] makeTiles(object bmap)
+        {
+            Bitmap bmp = (Bitmap)bmap;
+            Size tilesize = new Size(bmp.Width / 4, bmp.Height / 3);
+            Bitmap[,] bmparray = new Bitmap[4, 3];
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    //srcTile
+                    Rectangle movingTile = new Rectangle(i * tilesize.Width, j * tilesize.Height, tilesize.Width, tilesize.Height);
+
+                    bmparray[i, j] = new Bitmap(tilesize.Width, tilesize.Height);
+
+                    //ubacivanje slika u bmparray
+                    using (Graphics canvas = Graphics.FromImage(bmparray[i, j]))
+                    {
+                        canvas.DrawImage(bmp, new Rectangle(0, 0, tilesize.Width, tilesize.Height), movingTile, GraphicsUnit.Pixel);
                     }
                 }
-            });
+            }
+
+            return bmparray;
         }
 
-        return bmp;
-    }
-
-    public static Bitmap joinTiles(Bitmap[,] bitmaps, Bitmap original)
-    {
-        Size tilesize = new Size(original.Width / 4, original.Height / 3);
-        Bitmap res = new Bitmap(original.Width, original.Height);
-
-        for (int i = 0; i < 4; i++)
+        public static Bitmap[,] parallelImageProcess(Bitmap[,] bmp)
         {
-            for (int j = 0; j < 3; j++)
+
+            int broj_niti = Environment.ProcessorCount;
+            Console.WriteLine($"Dostupan broj niti je: {broj_niti}");
+            int worker_niti, io_niti;
+            ThreadPool.GetAvailableThreads(out worker_niti, out io_niti);
+
+            foreach (Bitmap bitmap in bmp)
             {
-                //srcTile
-                Rectangle movingTile = new Rectangle(0, 0, tilesize.Width, tilesize.Height);
-
-
-                //ubacivanje slika u rezultujucu mapu
-                using (Graphics canvas = Graphics.FromImage(res))
+                ThreadPool.QueueUserWorkItem((state) =>
                 {
-                    canvas.DrawImage(bitmaps[i,j], new Rectangle(i * tilesize.Width, j * tilesize.Height, tilesize.Width, tilesize.Height), movingTile, GraphicsUnit.Pixel);
+                    lock (bitmap)
+                    {
+                        int width = bitmap.Width;
+                        int height = bitmap.Height;
+
+                        for (int i = 0; i < width; i++)
+                        {
+                            for (int j = 0; j < height; j++)
+                            {
+                                Color oldPixel = bitmap.GetPixel(i, j);
+
+                                int grayScale = (int)((oldPixel.R * 0.229) + (oldPixel.G * 0.587) + (oldPixel.B * 0.114));
+                                Color newPixel = Color.FromArgb(grayScale, grayScale, grayScale);
+
+                                bitmap.SetPixel(i, j, newPixel);
+                            }
+                        }
+                    }
+                });
+            }
+            return bmp;
+        }
+
+        public static Bitmap joinTiles(Bitmap[,] bitmaps, Bitmap original)
+        {
+            Size tilesize = new Size(original.Width / 4, original.Height / 3);
+            Bitmap res = new Bitmap(original.Width, original.Height);
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    //srcTile
+                    Rectangle movingTile = new Rectangle(0, 0, tilesize.Width, tilesize.Height);
+
+
+                    //ubacivanje slika u rezultujucu mapu
+                    using (Graphics canvas = Graphics.FromImage(res))
+                    {
+                        canvas.DrawImage(bitmaps[i, j], new Rectangle(i * tilesize.Width, j * tilesize.Height, tilesize.Width, tilesize.Height), movingTile, GraphicsUnit.Pixel);
+                    }
                 }
             }
+
+            return res;
         }
 
-        return res;
-    }
+        static void Main(string[] args)
+        {
+            ServerDivided();
+        }
 
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Krece glavna nit...");
-        ServerDivided();
     }
 
 }
